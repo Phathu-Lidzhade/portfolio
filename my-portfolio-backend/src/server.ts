463d47265ac213fbd1919b03fc941e01213841ac
Phathu <maddangerchris@gmail.com>
2026-08-27 01:10:05 +0200

    created the backend of the project

---
+++ my-portfolio-backend/src/server.ts
@@ -0,0 +1,21 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+const PORT = 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/api/health", (_req, res) => {
+  res.json({
+    success: true,
+    message: "Portfolio backend is running",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+  console.log("Portfolio backend is running");
+});
